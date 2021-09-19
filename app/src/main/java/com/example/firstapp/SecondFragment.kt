d748576125a51441550e550db59e96873e4828ae
OstapSav <savka.ostap@israelit.pro>
2021-09-19 16:39:46 +0300

    Created my first app

--- app/src/main/java/com/example/firstapp/SecondFragment.kt
+++ app/src/main/java/com/example/firstapp/SecondFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.firstapp.databinding.FragmentSecondBinding
+import androidx.navigation.fragment.navArgs
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -18,6 +19,7 @@ class SecondFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val args: SecondFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,10 +37,23 @@ class SecondFragment : Fragment() {
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+        calculateRandomNumber()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun calculateRandomNumber() {
+        val count = args.myArg
+        val countText = getString(R.string.random_heading, count)
+        binding.textviewHeader.text = countText
+        val random = java.util.Random()
+        var randomNumber = 0
+        if (count > 0) {
+            randomNumber = random.nextInt(count + 1)
+        }
+        binding.textviewRandom.text = randomNumber.toString()
     }
 }
